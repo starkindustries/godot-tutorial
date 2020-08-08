70fa1f15f65360a9a932b705c4753cc5c850572d
Zion <zionperez@gmail.com>
2020-08-08 12:13:21 -0600

    First commit.

---
+++ Player/Player.gd
@@ -0,0 +1,28 @@
+extends KinematicBody2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+var velocity = Vector2.ZERO
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	print("Hello world")
+
+func _physics_process(delta):
+	var input_vector = Vector2.ZERO
+	input_vector.x = Input.get_action_strength(("ui_right")) - Input.get_action_strength(("ui_left"))
+	input_vector.y = Input.get_action_strength(("ui_down")) - Input.get_action_strength(("ui_up"))
+	
+	velocity = input_vector
+	#if input_vector != Vector2.ZERO:
+		#velocity = input_vector
+	#else:
+		#velocity = Vector2.ZERO
+	move_and_collide(velocity)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
