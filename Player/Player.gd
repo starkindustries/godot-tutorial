11317eac00ee08415715bb3c1978659e90a9241c
Zion <zionperez@gmail.com>
2020-08-08 12:57:21 -0600

    Complete part 3 in tutorial series.

--- Player/Player.gd
+++ Player/Player.gd
@@ -5,6 +5,9 @@ extends KinematicBody2D
 # var a = 2
 # var b = "text"
 
+const ACCELERATION = 500
+const MAX_SPEED = 100
+const FRICTION = 500
 var velocity = Vector2.ZERO
 
 # Called when the node enters the scene tree for the first time.
@@ -12,16 +15,26 @@ func _ready():
 	print("Hello world")
 
 func _physics_process(delta):
+	# TODO: Fix jitter and stutter:
+	# https://docs.godotengine.org/uk/latest/tutorials/misc/jitter_stutter.html
+	# https://youtu.be/EQA9MJ5_TxU?t=922
+	
+	# delta is time spent to calculate frame
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength(("ui_right")) - Input.get_action_strength(("ui_left"))
 	input_vector.y = Input.get_action_strength(("ui_down")) - Input.get_action_strength(("ui_up"))
 	
-	velocity = input_vector
-	#if input_vector != Vector2.ZERO:
-		#velocity = input_vector
-	#else:
-		#velocity = Vector2.ZERO
-	move_and_collide(velocity)
+	# https://www.youtube.com/watch?v=EQA9MJ5_TxU
+	# normalized()
+	# Returns the vector scaled to unit length. Equivalent to v / v.length().
+	input_vector = input_vector.normalized()	
+	
+	if input_vector != Vector2.ZERO:
+		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
+	else:
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+		
+	velocity = move_and_slide(velocity)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
