78b4b1b0a9ccd15af559cbf8cdee718fd622ebcc
Zion <zionperez@gmail.com>
2020-08-08 22:54:30 -0600

    Complete part 9.

--- Player/Player.gd
+++ Player/Player.gd
@@ -8,16 +8,29 @@ extends KinematicBody2D
 const ACCELERATION = 500
 const MAX_SPEED = 100
 const FRICTION = 500
+
+enum { MOVE, ROLL, ATTACK }
+
+var state = MOVE
 var velocity = Vector2.ZERO
-onready var animation_player = $AnimationPlayer
+# onready var animationPlayer = $AnimationPlayer
 onready var animationTree = $AnimationTree
 onready var animationState = animationTree.get("parameters/playback")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	animationTree.active = true
 		
 func _physics_process(delta):
+	match state:
+		MOVE:
+			move_state(delta)
+		ROLL:
+			pass
+		ATTACK:
+			attack_state(delta)
+	
+func move_state(delta):	
 	# TODO: Fix jitter and stutter:
 	# https://docs.godotengine.org/uk/latest/tutorials/misc/jitter_stutter.html
 	# https://youtu.be/EQA9MJ5_TxU?t=922
@@ -35,6 +48,7 @@ func _physics_process(delta):
 	if input_vector != Vector2.ZERO:
 		animationTree.set("parameters/Idle/blend_position", input_vector)
 		animationTree.set("parameters/Run/blend_position", input_vector)
+		animationTree.set("parameters/Attack/blend_position", input_vector)
 		animationState.travel("Run")
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 	else:
@@ -42,7 +56,16 @@ func _physics_process(delta):
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 		
 	velocity = move_and_slide(velocity)
+	
+	if Input.is_action_just_pressed("attack"):
+		state = ATTACK
 
+func attack_state(delta):
+	velocity = move_and_slide(velocity)
+	animationState.travel("Attack")
+	
+func attack_animation_finished():
+	state = MOVE
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
