3c4a2d85afe4d46062aff75b19ef9570610e65f6
Zion <zionperez@gmail.com>
2020-08-09 22:10:38 -0600

    Complate part 14.

--- Enemies/Bat.gd
+++ Enemies/Bat.gd
@@ -2,10 +2,19 @@ extends KinematicBody2D
 
 const FRICTION = 200
 var knockback = Vector2.ZERO
+onready var stats = $Stats
+
+func _ready():
+	print(stats.health)
 
 func _physics_process(delta):
 	knockback = knockback.move_toward(Vector2.ZERO, FRICTION * delta)
 	knockback = move_and_slide(knockback)
 
 func _on_HurtBox_area_entered(area):
+	stats.health -= area.damage
 	knockback = area.knockback_vector * 120
+
+
+func _on_Stats_no_health():
+	queue_free()
